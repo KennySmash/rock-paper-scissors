6e384c4915576d5dd72587e083af311dbb0f9eec
Kenny Webb <7831621+KennySmash@users.noreply.github.com>
2025-10-16 01:29:57 +0100

    cleaned up the flow of the program

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,21 @@
+// moving some properties into a top level object
+let gameState = {
+   choiceButton:  {},
+   log:           {},
+   gamesLeft:      0,
+   humanScore:     0,
+   compScore:      0
+}
 
+function setup(){
+   //Setting up the choice button
+   gameState.choiceButton = document.querySelector("#choiceButton");
 
-
+   gameState.choiceButton.addEventListener('click', ()=>{
+      // Tells the game we wanna start with 5 rounds
+      playGame(5);
+   })
+}
 
 function getComputerChoice() {
    let number = Math.random();
@@ -11,96 +26,92 @@ function getComputerChoice() {
    } else {
       return "scissor";
    }
-
-
 }
 
-console.log((getComputerChoice()));
+//Making the only thing this function does - collecting the player choice
+function getHumanChoice() {
+   let choice = prompt("Whats your choice?");
 
+   if (["rock", "r"].includes(humanChoice)) return "rock";
+   if (["paper", "p"].includes(humanChoice)) return "paper";
+   if (["scissors", "s"].includes(humanChoice)) return "scissors";
 
-function playGame(){
-   let humanScore = 0
-   let computerScore = 0
- for (let i = 0; i <5; i++){
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    roundResult = playRound(humanSelection,computerSelection,callback)
- function getHumanChoice() {
-   const choiceButton = document.querySelector("#choiceButton");
-   const log = document.querySelector("#log");
-
-   choiceButton.addEventListener("click", () => {
-      let choice = prompt("Whats your choice?");
-      const computerChoice = getComputerChoice()
-
-      alert(playRound(choice,computerChoice, function(){console.log({"humanScore":humanScore,"Comp":computerScore,"MyHand":choice,"TheirHand":computerChoice});}));
-
-
-      
-   })
-   
-} 
+   return null;
 }
-console.log(getHumanChoice());
 
- 
-
-function playRound(humanChoice, computerChoice, callback) {
-   const normalizedhumanChoice = String(humanChoice).toLowerCase();
-
-   if (["rock","r"].includes(humanChoice)) return "rock";
-   if (["paper","p"].includes(humanChoice)) return "paper";
-   if (["scissors","s"].includes(humanChoice)) return "scissors";
-
-   switch (normalizedhumanChoice) {
+// all this does is work out the game text
+function evaluateRound(human, comp, callback) {
+   switch (human) {
       case "rock":
          if (computerChoice === "scissors") {
-            humanScore++
-            callback();
             return "You win! Rock beats Scissors.";
          } else if (computerChoice === "paper") {
-            computerScore++
-            callback();
             return "You lose Paper beats Rock!";
          } else {
-            callback();
             return " It is a tie!"
          }
 
       case "paper":
-         if (computerChoice === "rock"){
-             humanScore++
-             callback();
+         if (computerChoice === "rock") {
             return "You win! Paper beats Rock.";
          } else if (computerChoice === "scissors") {
-             computerScore++
-             callback();
             return "You lose Scissors beats Paper!";
          } else {
-            callback();
             return " It is a tie!"
          }
       case "scissors":
-         if (computerChoice === "paper"){
-             humanScore++
-             callback();
+         if (computerChoice === "paper") {
             return "You win! Rock beats Scissors.";
          } else if (computerChoice === "rock") {
-             computerScore++
-             callback();
             return "You lose Rock beats Scissors!";
          } else {
-            callback();
             return " It is a tie!"
          }
       default:
-         callback();
          return "Invalid input. Please use 'Rock', 'Paper', 'Scissors'."
    }
-  
-} 
- return playGame
 }
 
-    
+// we pass in the number of rounds we wanna play
+function playGame(rounds) {
+   //we're setting up the number of rounds
+   gameState.gamesLeft = rounds
+
+   while(gameState.gamesLeft > 0){
+      //collect the human choice
+      humanSelection = getHumanChoice();
+      if (humanSelection === null){ console.error('something went very wrong with the humanChoice')}
+      
+      //get the bots play
+      computerSelection = getComputerChoice();
+
+      //calculate the round and get the result
+      let result = evaluateRound(humanSelection, computerSelection)
+      
+      // run the result though a compare so we can add to the scores
+      if (result.contains("You win")){
+         gameState.humanScore =+ 1;
+      } else if (result.contains("You lose")) {
+         gameState.compScore =+ 1;
+      }
+
+      // logging this single round
+      console.log({ 
+         "humanScore": gameState.humanScore, 
+         "Comp": cgameState.compScore, 
+         "MyHand": humanSelection, 
+         "TheirHand": computerSelection 
+      }); 
+
+      // Take a game off the gamesleft variable and loop back to the while
+      gameState.gamesLeft--;
+   }
+
+   // logging the full set of games
+   console.log('Here are the Results', "you won ", gameState.humanScore, "out of ", rounds, " games")
+   
+   //resetting the scoreboards
+   gameState.humanScore = 0;
+   gameState.computerScore = 0;
+}
 
